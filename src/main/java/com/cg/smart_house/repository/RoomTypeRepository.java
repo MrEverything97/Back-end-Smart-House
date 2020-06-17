ef92fe5170b6cae95ae4b34baeb932899e2073e5
loiminh <loiho11082015@gmail.com>
2020-06-17 16:18:38 +0700

    add RoomTypeServiceImpl

---
+++ src/main/java/com/cg/smart_house/repository/RoomTypeRepository.java
@@ -0,0 +1,7 @@
+package com.cg.smart_house.repository;
+
+import com.cg.smart_house.models.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTypeRepository extends JpaRepository<RoomType, Long> {
+}
