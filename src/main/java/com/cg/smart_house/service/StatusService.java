975c8329c25e87e0a2f015fb9ca023bc751494bb
loiminh <loiho11082015@gmail.com>
2020-06-17 16:25:52 +0700

    add StatusRepository

--- src/main/java/com/cg/smart_house/service/StatusService.java
+++ src/main/java/com/cg/smart_house/service/StatusService.java
@@ -1,4 +1,16 @@
 package com.cg.smart_house.service;
 
+import com.cg.smart_house.models.RoomType;
+import com.cg.smart_house.models.Status;
+
 public interface StatusService {
+    ServiceResult createStatus(Status status);
+
+    ServiceResult findAll();
+
+    ServiceResult findById(Long id);
+
+    ServiceResult deleteStatus(Long id);
+
+    ServiceResult updateStatus(Status status);
 }
---
+++ src/main/java/com/cg/smart_house/service/impl/StatusServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cg.smart_house.service.impl;
+
+import com.cg.smart_house.models.Status;
+import com.cg.smart_house.service.ServiceResult;
+import com.cg.smart_house.service.StatusService;
+
+public class StatusServiceImpl implements StatusService {
+    @Override
+    public ServiceResult createStatus(Status status) {
+        return null;
+    }
+
+    @Override
+    public ServiceResult findAll() {
+        return null;
+    }
+
+    @Override
+    public ServiceResult findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ServiceResult deleteStatus(Long id) {
+        return null;
+    }
+
+    @Override
+    public ServiceResult updateStatus(Status status) {
+        return null;
+    }
+}
