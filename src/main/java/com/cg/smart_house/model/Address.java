c6152b59e0e05df4be78732e62fc65175236c6c2
loiminh <loiho11082015@gmail.com>
2020-06-20 11:38:12 +0700

    Merge branch 'dev' of https://github.com/SmartHouseC1219G1/Back-end-Smart-House into dev.loi
    fig bug update Apartment
    # Conflicts:
    #	src/main/java/com/cg/smart_house/SmartHouseApplication.java
    #	src/main/java/com/cg/smart_house/controller/AddressController.java
    #	src/main/java/com/cg/smart_house/model/Address.java
    #	src/main/java/com/cg/smart_house/model/Apartment.java
    #	src/main/java/com/cg/smart_house/model/Category.java
    #	src/main/java/com/cg/smart_house/model/Order.java
    #	src/main/java/com/cg/smart_house/service/AddressService.java
    #	src/main/java/com/cg/smart_house/service/impl/AddressServiceImpl.java
    #	src/main/java/com/cg/smart_house/service/impl/ApartmentServiceImpl.java

---
+++ src/main/java/com/cg/smart_house/model/Address.java
@@ -0,0 +1,23 @@
+package com.cg.smart_house.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Province provinces;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "apartment_id", referencedColumnName = "id")
+    private Apartment apartment;
+}
