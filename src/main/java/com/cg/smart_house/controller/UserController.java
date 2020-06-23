e4b2f6b392e2410422a78a0b45fe70389f28f7df
dong <hoangdongfit@gmail.com>
2020-06-23 10:15:39 +0700

    update User

---
+++ src/main/java/com/cg/smart_house/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.cg.smart_house.controller;
+
+import com.cg.smart_house.model.Order;
+import com.cg.smart_house.model.User;
+import com.cg.smart_house.repository.UserRepository;
+import com.cg.smart_house.service.ServiceResult;
+import com.cg.smart_house.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PutMapping("/updateUser")
+    public ResponseEntity<ServiceResult> updateUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.updateUser(user), HttpStatus.OK);
+    }
+}
