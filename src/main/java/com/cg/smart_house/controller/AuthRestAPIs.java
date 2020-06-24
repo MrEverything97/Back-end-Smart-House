2e4ddf6f04f69182cd7d613e5a20554bd9106da3
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-25 00:21:44 +0700

    fix authorities return role list to front-end

--- src/main/java/com/cg/smart_house/controller/AuthRestAPIs.java
+++ src/main/java/com/cg/smart_house/controller/AuthRestAPIs.java
@@ -16,9 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -55,7 +59,8 @@ public class AuthRestAPIs {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = jwtProvider.generateJwtToken(authentication);
-        return ResponseEntity.ok(new JwtResponse(jwt));
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return ResponseEntity.ok(new JwtResponse(jwt,userDetails.getUsername(), (Collection<GrantedAuthority>) userDetails.getAuthorities()));
     }
 
     @PostMapping("/signUp")
--- src/main/java/com/cg/smart_house/message/responce/JwtResponse.java
+++ src/main/java/com/cg/smart_house/message/responce/JwtResponse.java
@@ -1,26 +1,50 @@
 package com.cg.smart_house.message.responce;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
+    private String username;
+    private Collection<GrantedAuthority> authorities;
 
-    public JwtResponse(String accessToken) {
+    public JwtResponse(String accessToken, String username, Collection<GrantedAuthority> authorities) {
         this.token = accessToken;
+        this.username = username;
+        this.authorities = authorities;
     }
 
-    public String getAccessToken() {
+    public String getToken() {
         return token;
     }
 
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
+    public void setToken(String token) {
+        this.token = token;
     }
 
-    public String getTokenType() {
+    public String getType() {
         return type;
     }
 
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Collection<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<GrantedAuthority> authorities) {
+        this.authorities = authorities;
     }
 }
